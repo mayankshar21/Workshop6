c02fe9163647a88cd940a2a6370b5337e43087d0
Mayank Sharma <mayankshar21@yahoo.com.sg>
2018-08-28 22:54:11 +1000

    Bills Edited

--- src/swen30006/workshop/state/Bills.java
+++ src/swen30006/workshop/state/Bills.java
@@ -1,5 +1,40 @@
 package swen30006.workshop.state;
 
+import java.util.ArrayList;
+
 public class Bills {
-	private boolean billPaid = true;
+	
+public AccountState accountState = AccountState.PENDINGSTATE; 
+	
+	private ArrayList<Product> productList;
+		
+	private ArrayList<Bills> billList;
+	
+	public void approval() {
+		
+	}
+	
+	public void billOutStanding() {
+		
+	}
+	
+	public void payBills() {
+		
+	}
+	
+	public void makeComplaint() {
+		
+	}
+	
+	public void complaintResolved() {
+		
+	}
+	
+	public void removeProduct() {
+		
+	}
+	
+	public void addProduct() {
+		
+	}
 }
